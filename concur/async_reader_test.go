9a2c09dfb4745e2158392cd2a301d69676ef043e
Chris Gibson <c.a.gibson1990@gmail.com>
2024-03-11 10:23:22 +0000

    initial tests

--- concur/async_reader_test.go
+++ concur/async_reader_test.go
@@ -3,8 +3,10 @@ package concur_test
 import (
 	"context"
 	"errors"
-	"log"
+	"reflect"
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/equalsgibson/concur/concur"
 )
@@ -17,53 +19,71 @@ func Test_AsyncReader_ValidateMessageOrder(t *testing.T) {
 		"Way",
 	}
 
-	// expectedData := mockData
-
 	counter := 0
 
 	mockReader := concur.NewAsyncReader[string](func(context.Context) (string, error) {
-		counter++
-		if len(mockData) > 0 {
-			result := mockData[0]
-			mockData = mockData[1:]
-
-			return result, nil
+		if len(mockData) == counter {
+			return "", errors.New("no more data")
 		}
 
-		return "", errors.New("no more data")
+		counter++
+
+		return mockData[counter-1], nil
 	})
 
 	ctx := context.Background()
 	go mockReader.Loop(ctx)
 
 	actualData := []string{}
-	go func() {
-		for update := range mockReader.Updates() {
-			if update.Err != nil {
-				t.Fatal(update.Err)
-			}
-			log.Println(update.Item)
 
-			actualData = append(actualData, update.Item)
+	for update := range mockReader.Updates() {
+		if update.Item == "" {
+			break
 		}
-	}()
 
-	for {
-		if counter != 4 {
-			continue
-		}
-		break
+		actualData = append(actualData, update.Item)
 	}
 
 	mockReader.Close()
 
-	t.Fatalf("%+v", actualData)
+	if !reflect.DeepEqual(actualData, mockData) {
+		t.Fatalf("expected updates to match original data - \nactualData:\t%+v,\nmockData:\t%+v", actualData, mockData)
+	}
 }
 
 func Test_AsyncReader_ValidateCleanup(t *testing.T) {
+	initalGoroutineCount := runtime.NumGoroutine()
 
-}
+	counter := 0
 
-func Test_AsyncReader_ValidateMultipleGoRountines(t *testing.T) {
+	mockReader := concur.NewAsyncReader[string](func(context.Context) (string, error) {
+		counter++
+		return "anything", nil
+	})
 
+	go func() {
+		for {
+			if counter >= 5 {
+				mockReader.Close()
+				break
+			}
+		}
+	}()
+
+	ctx := context.Background()
+	go mockReader.Loop(ctx)
+
+	for range mockReader.Updates() {
+		// Do nothing - just testing goroutines
+	}
+
+	time.Sleep(time.Millisecond * 50)
+
+	if runtime.NumGoroutine() > initalGoroutineCount {
+		t.Fatalf(
+			"Found %d goroutines running at the end of the test but we started with %d",
+			runtime.NumGoroutine(),
+			initalGoroutineCount,
+		)
+	}
 }
